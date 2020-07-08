02a7786cc21287a1fb929cff79f669f36cf5ce12
anton zam <anton_zam@live.se>
2020-07-08 23:55:26 +0200

    added new files for search algorithm

--- main.cpp
+++ main.cpp
@@ -3,16 +3,13 @@
 void print(std::vector<int>* vector)
 {
 	for (int i = 0; i < vector->size() ; i++)
-	{
-		
-		
+	{	
 			std::cout << (*vector)[i] << "\n";
-		
 	}
 }
 int main()
 {
-	int size = 22;
+	int size = 100;
 	auto a = find_primes(size);
 	auto b = create_container(a);
 	print(b);
